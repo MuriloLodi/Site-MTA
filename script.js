f31cb12319d47fd57c76329b3d8ec8fb6bf34a06
Murilo Henrique Lodi <132021915+MuriloLodi@users.noreply.github.com>
2024-10-19 21:26:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let btnMenu = document.getElementById('btn-menu')
+// Menu Mob Left
+let btnMenu = document.getElementById('abrirmenu')
 let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+let overlay = document.getElementById('ov')
 
 
 btnMenu.addEventListener('click', ()=>{
@@ -13,4 +14,23 @@ menu.addEventListener('click', ()=>{
 
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
+})
+
+// Menu Mob Right
+
+let btncart = document.getElementById('abrircart')
+let menu2 = document.getElementById('menucart')
+let overlay2 = document.getElementById('ov')
+
+
+btncart.addEventListener('click', ()=>{
+    menu2.classList.add('abrir-menu2')
+})
+
+menu2.addEventListener('click', ()=>{
+    menu2.classList.remove('abrir-menu2')
+})
+
+overlay2.addEventListener('click', ()=>{
+    menu2.classList.remove('abrir-menu2')
 })
