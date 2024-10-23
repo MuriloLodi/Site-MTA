0206e85201468a11c108325eec1d7d20b005657d
Murilo Henrique Lodi <132021915+MuriloLodi@users.noreply.github.com>
2024-10-22 23:46:16 -0300

    Site Finalizado - Primeiro Projeto Completo

--- script.js
+++ script.js
@@ -16,11 +16,10 @@ overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
 
-// Menu Mob Right
+// Menu PC Right
 
 let btncart = document.getElementById('abrircart')
-let menu2 = document.getElementById('menucart')
-let overlay2 = document.getElementById('ov')
+let menu2 = document.getElementById('cart-modal')
 
 
 btncart.addEventListener('click', ()=>{
@@ -31,6 +30,146 @@ menu2.addEventListener('click', ()=>{
     menu2.classList.remove('abrir-menu2')
 })
 
-overlay2.addEventListener('click', ()=>{
+overlay.addEventListener('click', ()=>{
     menu2.classList.remove('abrir-menu2')
 })
+
+
+
+let botaocart = document.getElementById('cart-btn')
+let menu3 = document.getElementById('cart-modal')
+
+
+botaocart.addEventListener('click', ()=>{
+    menu3.classList.add('abrir-menu3')
+})
+
+menu3.addEventListener('click', ()=>{
+    menu3.classList.remove('abrir-menu3')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu3.classList.remove('abrir-menu3')
+})
+
+
+///
+
+
+const menudesk = document.getElementById("menuk")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const cartCounter = document.getElementById("cart-count")
+
+
+let cart = [];
+
+
+
+menudesk.addEventListener("click", function(event){
+  // console.log(event.target)
+  let parentButton = event.target.closest(".add-to-cart-btn")
+
+  if(parentButton){
+    const name = parentButton.getAttribute("data-name")
+    const price = parseFloat(parentButton.getAttribute("data-price"))
+    addToCart(name, price)
+  }
+
+})
+
+
+// Função para adicionar no carrinho
+function addToCart(name, price){
+  const existingItem = cart.find(item => item.name === name)
+
+  if(existingItem){
+   //Se o item já existe, aumenta apenas a quantidade + 1 
+   existingItem.quantity += 1;
+
+  }else{
+
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    })
+
+  }
+
+  updateCartModal()
+
+}
+
+
+//Atualiza o carrinho
+function updateCartModal(){
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
+
+  cart.forEach(item => {
+    const cartItemElement = document.createElement("div");
+
+
+    cartItemElement.innerHTML = `
+      <div class="flex items-center justify-between">
+        <div>
+          <p>${item.name}</p>
+          <p>Qtd: ${item.quantity}</p>
+          <p>R$ ${item.price.toFixed(2)}</p>
+        </div>
+
+
+        <button class="remove-from-cart-btn" data-name="${item.name}">
+          Remover
+        </button>
+
+      </div>
+    `
+
+    total += item.price * item.quantity;
+
+    cartItemsContainer.appendChild(cartItemElement)
+
+  })
+
+  cartTotal.textContent = total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  });
+
+
+
+}
+
+
+// Função para remover o item do carrinho
+cartItemsContainer.addEventListener("click", function (event){
+  if(event.target.classList.contains("remove-from-cart-btn")){
+    const name = event.target.getAttribute("data-name")
+
+    removeItemCart(name);
+  }
+
+})
+
+function removeItemCart(name){
+  const index = cart.findIndex(item => item.name === name);
+
+  if(index !== -1){
+    const item = cart[index];
+    
+    if(item.quantity > 1){
+      item.quantity -= 1;
+      updateCartModal();
+      return;
+    }
+
+    cart.splice(index, 1);
+    updateCartModal();
+
+  }
+
+}
